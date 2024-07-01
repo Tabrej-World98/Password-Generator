7dca85f655a05f411956ba9c5c88e27319a9c986
Mohammed <78704469+ansarimohammed422@users.noreply.github.com>
2024-07-01 13:29:50 +0530

    Changed directory structure  README.me

---
+++ Techplement/Week 1/pass_character.py
@@ -0,0 +1,17 @@
+"""
+Module containing character sets for password generation.
+
+Attributes:
+    S_Letter (list): List of lowercase letters.
+    C_Letter (list): List of uppercase letters.
+    Symbol (list): List of special characters.
+    number (list): List of digits.
+"""
+
+S_Letter=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+C_Letter=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+Symbol=["~","`","!","#","%","^","&","*","(",")","-","_","+","=","{","}","[","]",":","|",";",'"',"'","<","@","$",",",">",".","?","/"]
+
+number=[1,2,3,4,5,6,7,8,9,0]
